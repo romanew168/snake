da31362fb1f0e56e23ec5c20fad9d35c9e8f571f
romanew168 <roma_new@mail.ru>
2020-04-23 23:08:31 +0700

    Добавил Hello Рома

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,7 +10,8 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello Рома");
+
             Console.ReadLine();
             
 
